cd500c37fe814895f2a9c719fee8a43aa59ba13f
jkilleran <johanventura71@gmail.com>
2022-12-13 13:39:25 -0400

    Agregando Reporta_tools v0.2

--- lib/views/reporta.dart
+++ lib/views/reporta.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:defensa_civil/views/reporta_tools/textformfield_edited.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
